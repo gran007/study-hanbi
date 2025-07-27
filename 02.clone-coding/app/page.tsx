478ebbfa0c052ae23ea77ad747571408485009d8
gran007 <gran007@naver.com>
2025-07-28 08:25:06 +0900

    change git ignore

---
+++ 02.clone-coding/app/page.tsx
@@ -0,0 +1,10 @@
+import style from './style.module.css'
+
+export default function Home() {
+  return (
+    <div className={style.body}>
+      <a className={style.link} href='/swivel'>Swivel</a>
+    </div>
+    
+  );
+}
