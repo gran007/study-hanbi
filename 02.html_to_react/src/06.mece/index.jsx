f796030a34687ee4daed929f74d8b3d67f332152
gran007 <gran007@naver.com>
2025-07-29 14:29:58 +0900

    code refactoring

---
+++ 02.html_to_react/src/06.mece/index.jsx
@@ -0,0 +1,13 @@
+const MECE = () => {
+  return (
+    <article className="info-block workshop-block">
+      <h3 className="block-title">MECE 소개</h3>
+      <h4 className="concept-title">가로 노드는 겹치지 않게, 부모 노드는 자식 노드의 요약</h4>
+      <div className="example-section">
+        <img src="./example.png" alt="MECE 원칙 예시" className="concept-image" />
+      </div>
+    </article>
+  )
+}
+
+export default MECE;
